d2b9fa2df9918eb015c6273bc58ef5e8174e7160
perkins1 <smperk71@gmail.com>
2016-08-18 18:19:03 -0400

    add canned search results with angular

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,14 +1,34 @@
-var CustomerSearchController = function($scope) {
-  $scope.search = function(searchTerm) {
-    $scope.searchedFor = searchTerm;
-  }
-}
+var app = angular.module('customers',[]); 
 
-app.controller("CustomerSearchController", [
-  "$scope",
-  function($scope) {
-    $scope.search = function(searchTerm) {
-      $scope.searchedFor = searchTerm;
-      }
+app.controller("CustomerSearchController", [ 
+          '$scope',                          
+  function($scope) {                         
+    $scope.customers = [];
+    $scope.search = function(searchTerm) {   
+      $scope.customers = [
+        {
+          "first_name":"Schuyler",
+          "last_name":"Cremin",
+          "email":"giles0@macgyver.net",
+          "username":"jillian0",
+          "created_at":"2015-03-04",
+        },
+        {
+          "first_name":"Derick",
+          "last_name":"Ebert",
+          "email":"lupe1@rennerfisher.org",
+          "username":"ubaldo_kaulke1",
+          "created_at":"2015-03-04",
+        },
+        {
+          "first_name":"Derick",
+          "last_name":"Johnsons",
+          "email":"dj@somewhere.org",
+          "username":"djj",
+          "created_at":"2015-03-04",
+        }
+      ]
+      
     }
-  ]);
+  }
+]);
