1ec3ded2066442459b63fd2efa5a78b7d2a13316
perkins1 <smperk71@gmail.com>
2016-08-18 20:33:36 -0400

    angularized the customer search

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,34 +1,18 @@
-var app = angular.module('customers',[]); 
+var app = angular.module("customers",[]); 
 
 app.controller("CustomerSearchController", [ 
-          '$scope',                          
-  function($scope) {                         
+          "$scope","$http",
+  function($scope , $http) {                         
     $scope.customers = [];
     $scope.search = function(searchTerm) {   
-      $scope.customers = [
-        {
-          "first_name":"Schuyler",
-          "last_name":"Cremin",
-          "email":"giles0@macgyver.net",
-          "username":"jillian0",
-          "created_at":"2015-03-04",
-        },
-        {
-          "first_name":"Derick",
-          "last_name":"Ebert",
-          "email":"lupe1@rennerfisher.org",
-          "username":"ubaldo_kaulke1",
-          "created_at":"2015-03-04",
-        },
-        {
-          "first_name":"Derick",
-          "last_name":"Johnsons",
-          "email":"dj@somewhere.org",
-          "username":"djj",
-          "created_at":"2015-03-04",
+      $http.get("/customers.json",  
+                { "params": { "keywords": searchTerm } }
+      ).then(function(response) {
+          $scope.customers = response.data;
+      },function(response) {
+          alert("There was a problem: " + response.status);
         }
-      ]
-      
+      );
     }
   }
 ]);
