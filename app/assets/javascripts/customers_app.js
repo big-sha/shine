2064dd12876f0fc64c925f359510dbfc090abf87
perkins1 <smperk71@gmail.com>
2016-08-22 08:53:18 -0400

    update angular search

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,18 +1,40 @@
-var app = angular.module("customers",[]); 
+var app = angular.module('customers',[]); 
 
 app.controller("CustomerSearchController", [ 
-          "$scope","$http",
+          '$scope','$http',
   function($scope , $http) {                         
+
+    var page = 0;
+
     $scope.customers = [];
     $scope.search = function(searchTerm) {   
+      $scope.loading = true;
+      if (searchTerm.length < 3) {
+        return;
+      }
+      // ... rest of the function
       $http.get("/customers.json",  
-                { "params": { "keywords": searchTerm } }
-      ).then(function(response) {
-          $scope.customers = response.data;
-      },function(response) {
-          alert("There was a problem: " + response.status);
-        }
-      );
+                { "params": { "keywords": searchTerm, "page": page } }
+      ).success(
+        function(data,status,headers,config) { 
+          $scope.customers = data;
+          $scope.loading = false;
+      }).error(
+        function(data,status,headers,config) {
+          $scope.loading = false;
+          alert("There was a problem: " + status);
+        });
+    }
+
+    $scope.previousPage = function() {
+      if (page > 0) {
+        page = page - 1;
+        $scope.search($scope.keywords);
+      }
+    }
+    $scope.nextPage = function() {
+      page = page + 1;
+      $scope.search($scope.keywords);
     }
   }
 ]);
